f24b0cef53916285f096d93de77a99f116e8a570
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-07-01 10:41:37 +0300

    add dz2a

---
+++ DZ1/src/main/java/DZ2a.java
@@ -0,0 +1,26 @@
+public class DZ2a {
+    public static void main(String[] args) throws Exception {
+        String [] arrayData =  lib.ReadLineFromFile("C:\\Users\\79200\\Desktop\\javaDZ\\DZ1\\src\\main\\java\\txt");
+        for(int i = 0; i < arrayData.length; i++) {
+            System.out.println(PrintLine(arrayData[i]));
+        }
+
+    }
+    public static StringBuilder PrintLine(String line) {
+        String line1 = line.replace("{", "");
+        String line2 = line1.replace("}", "");
+        String line3 = line2.replaceAll("\"", "");
+        String line4 = line3.replace("[", "");
+        String line5 = line4.replace("]", "");
+        StringBuilder result = new StringBuilder("");
+        String [] arrayData = line5.split(",");
+        String [] listName = {"Студент ", " получил ", " по предмету "};
+        for (int i =0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            result.append(listName[i]);
+            result.append(arrData[1]);
+        }
+        return result;
+    }
+}
+
