fddcad9bd11312bac146e4c3e700e23ae76ca919
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-07-09 20:47:43 +0300

    add dz5

---
+++ DZ1/src/main/java/DZ5.java
@@ -0,0 +1,129 @@
+import java.util.*;
+public class DZ5 {
+
+
+    static void sortedPrint(Map<String, ArrayList> map) {
+
+
+        Set<String> keySet = map.keySet();
+
+
+        int maxCount = 0;
+        int minCount = 1_000_000;
+
+        for (var item : map.entrySet()) {
+            if (maxCount < item.getValue().size())
+                maxCount = item.getValue().size();
+            if (minCount > item.getValue().size())
+                minCount = item.getValue().size();
+
+        }
+
+        Stack<String> st = new Stack<>();
+        int num = minCount;
+        while (num <= maxCount) {
+            // System.out.println(map);
+            for (var item : map.entrySet()) {
+                if (item.getValue().size() == num) {
+                    st.push(item.getKey());
+                }
+            }
+            num += 1;
+        }
+
+        String name;
+        for (int i = 0; i < keySet.size(); i++) {
+            name = st.pop();
+            for (var item : map.entrySet()) {
+                if (name == item.getKey()) {
+                    System.out.printf("%8s: ", item.getKey());
+                    System.out.println(item.getValue());
+                }
+            }
+        }
+        System.out.println();
+    }
+
+
+        public static void main(String[] args) {
+
+        Map<String, ArrayList> abon = new HashMap<>() {
+            {
+                put("фффф", new ArrayList<Integer>() {
+                    {
+                        add(34634);
+                        add(1535);
+                        add(46835745);
+                    }
+                });
+                put("ыыыыы", new ArrayList<Integer>() {
+                    {
+                        add(555464);
+                    }
+                });
+                put("вввв", new ArrayList<Integer>() {
+                    {
+                        add(223662);
+                        add(4577524);
+
+                    }
+                });
+                put("ррррр", new ArrayList<Integer>() {
+                    {
+                        add(57648437);
+                        add(36587);
+
+                    }
+                });
+            }
+        };
+        System.out.println();
+        // Печатаем исходный набор данных
+        System.out.println("Исходные данные: ");
+        sortedPrint(abon);
+
+        // Создаем циклическое меню
+        Scanner scan = new Scanner(System.in, "cp866");
+        Boolean getOut = false;
+        String st;
+        while (!getOut) {
+            System.out.println("Введите номер действия (1 - добавить абонента, 9 - выйти из программы):");
+            st = scan.nextLine();
+            String name = "";
+            String phString;
+            switch (st) {
+                case "1": {
+                    System.out.println("Введите фамилию абонента:");
+                    name = scan.nextLine();
+                    if (abon.containsKey(name)) {
+                        System.out.println("Такая фамилия уже есть. Больше фантазии!");
+                        System.out.println();
+                        break;
+                    } else {
+                        System.out.println("Введите номера телефонов через запятую: ");
+                        phString = scan.nextLine();
+                        String[] arr = phString.split(",");
+                        ArrayList<Integer> arrInt = new ArrayList<>();
+                        for (String item: arr) {
+                            arrInt.add(Integer.parseInt(item.trim())) ;
+                        }
+                        abon.put(name, arrInt);
+                        System.out.println();
+                        sortedPrint(abon);
+                        break;
+                    }
+                }
+                case "9": {
+                    getOut = true;
+                    System.out.println();
+                    System.out.println("До свидания!");
+                    System.out.println();
+                    break;
+                }
+
+
+            }
+        }
+    }
+
+}
